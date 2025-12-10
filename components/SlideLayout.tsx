2dea099dd7892da0d59d19cb1874efe929f3f472
Ricardo Alexandre Brasil Júnior <104632797+Lawalash@users.noreply.github.com>
2025-12-10 18:37:12 -0300

    ajuste correção

--- components/SlideLayout.tsx
+++ components/SlideLayout.tsx
@@ -32,7 +32,7 @@ export const SlideLayout: React.FC<SlideLayoutProps> = ({
           src={timeLogo} 
           alt="Time Logo" 
           className="object-contain rotate-90"
-          style={{ max-width: '300%', height: 'auto', margin-bottom:70px }}
+          style={{ maxWidth: '300%', height: 'auto', marginBottom: '70px' }}
         />
         </aside>
       )}
